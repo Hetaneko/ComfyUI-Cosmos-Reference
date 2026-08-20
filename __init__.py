7c189122ec35742ce3056ef308df59bccdc1571b
Hetaneko <54069666+Hetaneko@users.noreply.github.com>
2026-08-20 20:11:21 +0300

    Add AnimaEditModelLoader to CosmosReferenceExtension

--- __init__.py
+++ __init__.py
@@ -5,13 +5,13 @@ except ImportError:
 
 from comfy_api.latest import ComfyExtension, io
 
-from .cosmos_temporal_reference import ApplyCosmosReferenceLatent
+from .cosmos_temporal_reference import ApplyCosmosReferenceLatent, AnimaEditModelLoader
 
 
 class CosmosReferenceExtension(ComfyExtension):
     @override
     async def get_node_list(self) -> list[type[io.ComfyNode]]:
-        return [ApplyCosmosReferenceLatent]
+        return [ApplyCosmosReferenceLatent, AnimaEditModelLoader]
 
 
 async def comfy_entrypoint():
